e066b926568f663b94abbfe286b924e948b383d9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_BindingToXML/Window1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Data;
+
+namespace DXGrid_BindingToXML {
+  
+    public partial class Window1 : Window {
+        static string path = @"..\..\Contacts.xml";
+        public Window1() {
+            InitializeComponent();
+            grid.ItemsSource = GetDataFromXML();
+        }
+        private DataTable GetDataFromXML() {
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            return ds.Tables[0];
+        }
+        private void PostDataToXML() {
+            ((DataTable)grid.ItemsSource).DataSet.WriteXml(path);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            PostDataToXML();
+            MessageBox.Show("Changes have been successfully saved to an XML file.", "Info");
+        }
+    }
+   
+}
---
+++ VB/DXGrid_BindingToXML/Window1.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows
+Imports System.Data
+
+Namespace DXGrid_BindingToXML
+
+    Partial Public Class Window1
+        Inherits Window
+
+        Private Shared path As String = "..\..\Contacts.xml"
+        Public Sub New()
+            InitializeComponent()
+            grid.ItemsSource = GetDataFromXML()
+        End Sub
+        Private Function GetDataFromXML() As DataTable
+            Dim ds As New DataSet()
+            ds.ReadXml(path)
+            Return ds.Tables(0)
+        End Function
+        Private Sub PostDataToXML()
+            CType(grid.ItemsSource, DataTable).DataSet.WriteXml(path)
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            PostDataToXML()
+            MessageBox.Show("Changes have been successfully saved to an XML file.", "Info")
+        End Sub
+    End Class
+
+End Namespace
