7af2c9f46108df35c70f88c380a03c9169cf600f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_BindingToXML/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace DXGrid_BindingToXML {
+    #region #1
+    public partial class Window1 : Window {
+        static string path = @"..\..\Contacts.xml";
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = GetDataFromXML();
+        }
+        private DataTable GetDataFromXML() {
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+            return ds.Tables[0];
+        }
+        private void PostDataToXML() {
+            ((DataTable)grid.DataSource).DataSet.WriteXml(path);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            PostDataToXML();
+            MessageBox.Show("Changes have been successfully saved to an XML file.", "Info");
+        }
+    }
+    #endregion #1
+}
---
+++ VB/DXGrid_BindingToXML/Window1.xaml.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports System.Data
+
+Namespace DXGrid_BindingToXML
+	#Region "#1"
+	Partial Public Class Window1
+		Inherits Window
+		Private Shared path As String = "..\..\Contacts.xml"
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = GetDataFromXML()
+		End Sub
+		Private Function GetDataFromXML() As DataTable
+			Dim ds As New DataSet()
+			ds.ReadXml(path)
+			Return ds.Tables(0)
+		End Function
+		Private Sub PostDataToXML()
+			CType(grid.DataSource, DataTable).DataSet.WriteXml(path)
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			PostDataToXML()
+			MessageBox.Show("Changes have been successfully saved to an XML file.", "Info")
+		End Sub
+	End Class
+	#End Region ' #1
+End Namespace
