642f6a9130cd86822b559fb06724c0de56ce7cae
DevExpressExampleBot <fakeEmail>
2022-03-30 16:02:26 +0300

    Source auto update [skip ci]

--- VB/DXGrid_BindingToXML/Window1.xaml.vb
+++ VB/DXGrid_BindingToXML/Window1.xaml.vb
@@ -1,23 +1,26 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Data
 
 Namespace DXGrid_BindingToXML
 
-    Partial Public Class Window1
+    Public Partial Class Window1
         Inherits Window
 
         Private Shared path As String = "..\..\Contacts.xml"
+
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = GetDataFromXML()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GetDataFromXML()
         End Sub
+
         Private Function GetDataFromXML() As DataTable
-            Dim ds As New DataSet()
+            Dim ds As DataSet = New DataSet()
             ds.ReadXml(path)
             Return ds.Tables(0)
         End Function
+
         Private Sub PostDataToXML()
-            CType(grid.ItemsSource, DataTable).DataSet.WriteXml(path)
+            CType(Me.grid.ItemsSource, DataTable).DataSet.WriteXml(path)
         End Sub
 
         Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
@@ -25,5 +28,4 @@ Namespace DXGrid_BindingToXML
             MessageBox.Show("Changes have been successfully saved to an XML file.", "Info")
         End Sub
     End Class
-
 End Namespace
